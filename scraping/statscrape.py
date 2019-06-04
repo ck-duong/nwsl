c5e56af31d111631b9746126f127e763b3f2d007
Casey Duong <ckduong14@gmail.com>
2019-06-04 12:42:29 -0700

    started EDA, scraped data into csvs

---
+++ scraping/statscrape.py
@@ -0,0 +1,49 @@
+"""Individual NWSL Player Stat Scraper
+
+This script allows the user to scrape player stat data
+from the official NWSL website from 2016 - 2019, but
+can be modified to scrape a different range of years.
+
+This script requires that the packages imported below be installed 
+within the Python environment you are running this script in.
+"""
+
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import numpy as np
+import time
+import os
+
+soccer = "http://www.nwslsoccer.com/stats?season={}#players"
+for i in range(2016, 2020):
+    linked = soccer.format(i)
+    pull = requests.get(linked)
+    html = pull.content
+    soup = BeautifulSoup(html, 'html.parser')
+
+    #print(soup.prettify())
+
+    stats = ['player-name', '', 'gp', 'gs', 'mins', 'g current', 'a',
+    's', 'sog', 'fc', 'off', 'ck', 'pka', 'pkg', 'yc', 'rc']
+    all_stats = []
+    for category in stats: #replace with function, apply. #TODO
+        statistic = soup.find_all("td", class_= category)
+        arr = []
+        for each in statistic:
+            stat = each.text.strip().replace('\n', ' ')
+            arr.append(stat)
+        all_stats.append(arr)
+
+    df = pd.DataFrame(all_stats)
+    df = df.transpose()
+    df.columns = ['Player Name', 'Team', 'Games Played', 'Games Started', 'Minutes Played', 'Goals',
+    'Assists', 'Shots', 'Shots on Goal', 'Fouls Committed', 'Offsides', 'Corner Kicks',
+    'Penalty Kicks Attempted', 'Penalty Kick Goals', 'Yellow Cards', 'Red Cards']
+    df = df.dropna() #remove team data
+
+    name = 'nwsl{}.csv'.format(i)
+    path = os.path.join('data', 'nwsl', name)
+
+    df.to_csv(path, index=False)
+    time.sleep(2)
--- scraping/teamscrape.py
+++ scraping/teamscrape.py
@@ -1,15 +1,26 @@
+"""NWSL Player Position Scraper
+
+This script allows the user to scrape player stat data
+from the official NWSL website, specifically from their
+team's roster page, from 2016 - 2019, but can be modified
+to scrape a different range of years.
+
+This script requires that the packages imported below be installed 
+within the Python environment you are running this script in.
+"""
+
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+import numpy as np
+import time
+import os
 
-#TODO: Get data by year
 base = 'http://www.nwslsoccer.com'
 teams = base + '/stats#teams'
 pull = requests.get(teams)
 html = pull.content
 soup = BeautifulSoup(html, 'html.parser')
-
-df = pd.DataFrame(columns = ['Player', 'Position'])
 
 team_links = []
 
@@ -17,21 +28,32 @@ team_data = soup.find(id="teams-nav")
 for link in team_data.find_all('a'):
     team_links.append(link.get('href'))
 
-for team in team_links:
-    team_url = base + team
-    team_pull = requests.get(team_url)
-    team_content = team_pull.content
-    team_soup = BeautifulSoup(team_content, 'html.parser')
+for i in range(2016, 2020):
+    df = pd.DataFrame(columns = ['Player', 'Position'])
     
-    roster = team_soup.find('table', 
-                            {'class': 'table table-responsive roster-table nwsl-table col-xs-12 col-sm-12 col-md-12 col-lg-12'})
-    team_stats = []
-    statistic = roster.find_all('td', class_ = 'player-name')
-    
-    for each in statistic:
-        stat = each.text
-        team_stats.append(stat.rsplit(' ', 1))
-    team_df = pd.DataFrame(team_stats, columns = ['Player', 'Position'])
-    df = pd.concat([df, team_df], ignore_index=True)
-    
-df.to_csv('position.csv', index = False)
+    for team in team_links:
+        team_url = "http://www.nwslsoccer.com{}?statsType=team&statsSeason=2019&scheduleSeason=2019&rosterSeason={}&psort=#roster".format(team, i)
+        team_pull = requests.get(team_url)
+        
+        team_content = team_pull.content
+        team_soup = BeautifulSoup(team_content, 'html.parser')
+        if team_soup.find('h2', {'class' : 'error__code'}):
+            break
+                
+        roster = team_soup.find('table', 
+                                {'class': 'table table-responsive roster-table nwsl-table col-xs-12 col-sm-12 col-md-12 col-lg-12'})
+        team_stats = []
+        statistic = roster.find_all('td', class_ = 'player-name')
+
+        for each in statistic:
+            stat = each.text
+            team_stats.append(stat.rsplit(' ', 1))
+            
+        team_df = pd.DataFrame(team_stats, columns = ['Player', 'Position'])
+        df = pd.concat([df, team_df], ignore_index=True)
+
+        name = 'position{}.csv'.format(i)
+        path = os.path.join('data', 'position', name)
+
+        df.to_csv(path, index=False)
+        time.sleep(2)
